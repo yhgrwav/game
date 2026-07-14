97e7b0e6173dbc491d398cd92e1b34ce653ed195
yeahgor <yeahgorq@gmail.com>
2026-07-14 03:38:27 +0300

    feat: added classes list

---
+++ internal/catalog/classes.go
@@ -0,0 +1,9 @@
+package catalog
+
+import "game/internal/entity"
+
+var Classes = []entity.ClassInfo{
+	{Class: entity.Warrior, Title: "Воин"},
+	{Class: entity.Barbarian, Title: "Варвар"},
+	{Class: entity.Robber, Title: "Разбойник"},
+}
