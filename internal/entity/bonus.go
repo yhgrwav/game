e1f1befceb80493ff1c7b52a5296b5a8ae2eb3df
yeahgor <yeahgorq@gmail.com>
2026-07-14 03:36:53 +0300

    feat: +bonus entity

---
+++ internal/entity/bonus.go
@@ -0,0 +1,27 @@
+package entity
+
+// Logic - модификатор урона. Всё, что нужно для решения, бонус берёт из FightContext:
+// кто бьёт, кого бьют и какой сейчас ход
+type Logic func(ctx FightContext, damage uint16) uint16
+
+type BonusName string
+
+const (
+	Rage            BonusName = "rage"
+	StoneSkin       BonusName = "stone_skin"
+	HiddenAttack    BonusName = "hidden_attack"
+	Shield          BonusName = "shield"
+	ImpulseToAction BonusName = "impulse_to_action"
+	Poison          BonusName = "poison"
+	AgilityUp       BonusName = "agility_up"
+	StrengthUp      BonusName = "strength_up"
+	StaminaUp       BonusName = "stamina_up"
+)
+
+type Bonus struct {
+	Title         string
+	Description   string
+	Income        Logic
+	Outcome       Logic
+	AttributeGain func(h *Hero)
+}
